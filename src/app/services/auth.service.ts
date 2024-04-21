826db30fd17f55eb9868aa3922dd611d96cbce46
neverloved-dev <djordjeandjic99@gmail.com>
2024-04-21 19:23:13 +0100

    call the http

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { UserLoginDTO } from "../auth/shared_DTOs/user-login.dto";
 import { UserRegisterDTO } from "../auth/shared_DTOs/user-register.dto";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({providedIn:'root'})
 export class AuthService{
@@ -9,10 +10,10 @@ export class AuthService{
         
     }
     logIn(userLoginDTO:UserLoginDTO){
-        
+        return this.http.post(this.baseUrl+"/login",userLoginDTO);
     }
 
     signup(userRegisterDTO:UserRegisterDTO){
-
+        return this.http.post(this.baseUrl+"/register",userRegisterDTO);
     }
 }
