48da089fc7d9a6f52c0b8c24703048cb8f089d86
neverloved-dev <djordjeandjic99@gmail.com>
2024-05-15 16:58:20 +0100

    need to work on saving logic

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,23 +1,26 @@
-import { Injectable } from "@angular/core";
+import {Inject, Injectable} from "@angular/core";
 import { UserLoginDTO } from "../auth/shared_DTOs/user-login.dto";
 import { UserRegisterDTO } from "../auth/shared_DTOs/user-register.dto";
 import { HttpClient } from "@angular/common/http";
+import {LocalStorageService} from "./localStorageService.service";
 
 @Injectable({providedIn:'root'})
 export class AuthService{
+    @Injectable()
     protected baseUrl = "localhost://5632";
-    constructor(private http: HttpClient){
+    constructor(private http: HttpClient,private localStorageService:LocalStorageService){
         
     }
     logIn(userLoginDTO:UserLoginDTO){
-        return this.http.post(this.baseUrl+"/login",userLoginDTO);
+        var key = this.http.post(this.baseUrl+"/login",userLoginDTO);
+        this.localStorageService.set("Bearer",key)
     }
 
     signup(userRegisterDTO:UserRegisterDTO){
         return this.http.post(this.baseUrl+"/register",userRegisterDTO);
     }
 
-    getJwtToken(){
-      var key = 
+    getJwtToken():string{
+     return "";
     }
 }
